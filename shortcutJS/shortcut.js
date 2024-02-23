c45c751e88f050787c08895e1f697ae9a1cf1e88
XHiddenProjects <64079569+XHiddenProjects@users.noreply.github.com>
2024-02-22 21:30:51 -0500

    Update shortcut.js

--- shortcutJS/shortcut.js
+++ shortcutJS/shortcut.js
@@ -1,7 +1,7 @@
 /*
 @Author XHiddenProjects
-@Version 1.0.6
-@Updated 11/11/23
+@Version 1.1.0
+@Updated 02/22/24
 */
 const SHIFT = true;
 const CTRL = true;
@@ -149,4 +149,3 @@ class shortcutJS{
 		}
 	}
 }
-export {shortcutJS, SHIFT, CTRL, ALT, NO_SHIFT, NO_CTRL, NO_ALT} 
